80337c96a19e3f778291b723fa5595322aa0701b
ctsaguen <cedric.tsague_ngou@etud.univ-jfc.fr>
2020-03-14 21:28:18 +0100

    gestion des allUnlocks

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { RestService } from './service/rest.service';
 import { NotificationService } from './service/notification.service';
-
+import { ProductsComponent } from './products/products.component'
 
 import { World } from './model/world.model';
 import { Product } from './model/product.model';
@@ -14,12 +14,14 @@ import { Pallier } from './model/pallier.model'
 })
 
 export class AppComponent {
+  @ViewChildren(ProductsComponent) public productsComponent: QueryList<ProductsComponent>;
   title = 'client';
   username: string = '';
   qtmulti: number = 1;
   world: World = new World();
   server: string;
   dispoManager:boolean;
+
   constructor(private service: RestService, private notifyService : NotificationService) {
     this.server = service.getServer();
     service.getWorld().then(world => {
@@ -29,17 +31,19 @@ export class AppComponent {
     this.createUsername();
   }
 
+  //ici on teste la disponibilité d'un manager pour le signaler visuellement à l'utilisateur d'un changement et c'est utilisé dans lors des nombreuses productions.
   disponibiliteManager():void{
    this.dispoManager = false;
     this.world.managers.pallier.forEach(val => {
       if(!this.dispoManager){
-        if(this.world.money > val.seuil){
+        if(this.world.money > val.seuil && !val.unlocked){
           this.dispoManager = true;
          }
       }
     })
   }
 
+  //commutateur de changement des valeurs possible d'achat de produit, on a soit 1 produit, soit 10, soit 100 ou le max
   commutateur() {
     switch (this.qtmulti) {
       case 1:
@@ -49,26 +53,30 @@ export class AppComponent {
         this.qtmulti = 100
         break;
       case 100:
-        this.qtmulti = 1000
+        this.qtmulti = 100000
         break;
       default:
         this.qtmulti = 1
     }
   }
+  //Prise en compte des productions effectués dans le component Product
   onProductionDone(p: Product) {
     this.world.money = this.world.money + p.revenu;
     this.world.score = this.world.score + p.revenu;
     this.disponibiliteManager();
   }
 
+  //on valide l'achat d'un produit dans le component Product
   onAchatDone(m: number) {
     this.world.money = this.world.money - m;
   }
 
+  //ici on enregistre les changements de nom d'utilisateur effectué par l'utilisateur
   onUsernameChanged(): void {
     localStorage.setItem("username", this.username);
     this.service.setUser(this.username);
   }
+  //ici on crée le nom d'utilisateur s'il n'exite pas et l'enregistre dans le serveur
   createUsername(): void {
     this.username = localStorage.getItem("username");
     if (this.username == '') {
@@ -78,6 +86,7 @@ export class AppComponent {
     this.service.setUser(this.username);
   }
   
+  //cette partie nous permet d'acheter un manager 
   achatManager(m : Pallier){
    if(this.world.money >= m.seuil){
       this.world.money = this.world.money - m.seuil;
--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { NotificationService } from '../service/notification.service';
 
 import { Product } from '../model/product.model';
+import { Pallier } from '../model/pallier.model'
 
 declare var require;
 var ProgressBar = require('progressbar.js');
@@ -16,6 +18,10 @@ export class ProductsComponent implements OnInit {
   server: string = 'http://localhost:8080/';
   isRun: boolean;
   bar: any;
+  //cette variable sert à faire évoluer les seuils de bonus
+  seuil : number;
+
+  //on récupére le produit du world
   product: Product;
   @Input()
   set prod(value: Product) {
@@ -27,24 +33,25 @@ export class ProductsComponent implements OnInit {
       this.bar.animate(1, { duration: this.product.timeleft });
     }
   }
-
+  //on récupère les gains du joueur 
   _money: number;
   @Input()
   set money(value: number) {
     this._money = value;
   }
-
+  //on récupère la valeur du commutateur de quantité d'achat
   _qtmulti: number;
   @Input()
   set qtmulti(value: number) {
     this._qtmulti = value;
     if (this._qtmulti && this.product) this.calcMaxCanBuy();
   }
-
+  //on renvoie à la couche mère le le Product en production 
   @Output() notifyProduction: EventEmitter<Product> = new EventEmitter<Product>();
+  //on renvoie à la couche mère le coût total du produit acheté 
   @Output() notifyMoney: EventEmitter<number> = new EventEmitter<number>();
 
-  constructor() { }
+  constructor(private notifyService : NotificationService) { }
 
 
   ngOnInit(): void {
@@ -54,6 +61,7 @@ export class ProductsComponent implements OnInit {
   }
 
   ngAfterViewInit() {
+    //ici on a mis un timeout afin d'initialiser la progressbar après la création du DOM sinon cela ne fonctionne pas. le hook se lance trop tôt
     setTimeout(() => {
       this.bar = new ProgressBar.Line(this.progressBarItem.nativeElement, {
         strokeWidth: 4,
@@ -71,7 +79,7 @@ export class ProductsComponent implements OnInit {
     }, 100)
 
   }
-
+  //fonction de production utilisé quand le joueur lance une production
   production() {
     // if (this.product.quantite >= 1) {
     let progress = (this.product.vitesse - this.product.timeleft) / this.product.vitesse;
@@ -81,7 +89,7 @@ export class ProductsComponent implements OnInit {
     this.isRun = true;
     //  }
   }
-
+  //calcul du score du joueur après une production, elle est lancé chaque 100ms par le hook ngOnInit et mis à jour les resultats 
   calcScore() {
     if (this.isRun) {
       if (this.product.timeleft > 0) {
@@ -100,6 +108,7 @@ export class ProductsComponent implements OnInit {
     }
   }
 
+  //cette fonction calcul la quantité maximal que que le joueur peut acheter en fonction de son argent
   calcMaxCanBuy(): number {
     var a = 1 - ((this._money / this.product.cout) * (1 - this.product.croissance)) - this.product.croissance;
     console.log(a)
@@ -109,12 +118,32 @@ export class ProductsComponent implements OnInit {
     return maxAchat;
   }
 
+  // cette fonction lance une production
   achatProduct() {
     console.log(this.calcMaxCanBuy())
     //if(this._qtmulti >= this.calcMaxCanBuy()){
-    var coutAchat = this.product.cout * this._qtmulti
+    var coutAchat = this.product.cout * this._qtmulti;
+    this.product.quantite = this.product.quantite++;
     this.notifyMoney.emit(coutAchat);
     // }
+  }
+  
+  //ici on calcul si le joeur a débloqué un bonus et il y'a 3 type de bonus, à savoir vitesse, gain ou encore ange
+  calcUpgrade(pallier: Pallier) {
+    switch (pallier.typeratio) {
+      case 'vitesse':
+        this.product.vitesse = this.product.vitesse / pallier.ratio;
+        break;
+      case 'gain':
+        this.product.revenu = this.product.revenu * pallier.ratio;
+        break;
+      case 'ange':
+        console.log('ange');
+        break;
+      default:
+        break;
+    }
+    this.notifyService.showSuccess("déblocage d'un bonus "+pallier.typeratio+"effectué pour "+this.product.name, "BONUS")
   }
 
 }
